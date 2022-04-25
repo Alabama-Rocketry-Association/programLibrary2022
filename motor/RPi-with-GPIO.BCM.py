eb5eea8aa7aeea400a47fa018a40d7089cc28da8
Avery Fernandez <88298311+OptimumAF@users.noreply.github.com>
2022-04-25 15:38:42 -0500

    Create RPi-with-GPIO.BCM.py

---
+++ motor/RPi-with-GPIO.BCM.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+DIR = 23 # PIN NUMBERS
+ENABLE = 24 # PIN NUMBERS
+STEP = 25 # PIN NUMBERS
+
+CW = 1  # CW is backward
+CCW = 0  # CCW is forward
+
+GPIO.setmode(GPIO.BCM) # allows for the pin layout that raspi uses
+
+# sets the pins for the different data
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.setup(ENABLE, GPIO.OUT)
+
+GPIO.output(DIR, CW)  # presets direction
+GPIO.output(ENABLE, GPIO.HIGH)  # stops power draw
+
+
+while True:
+    sleep(1.0)  # unnecessary i think
+    GPIO.output(DIR, CW)  # sets the direction, give it time before switching direction
+    for x in range(400):  # change the range for how long it runs
+        GPIO.output(STEP, GPIO.HIGH)  # alternates to allow the stepper to run
+
+        sleep(.001)  # do not change
+
+        GPIO.output(STEP, GPIO.LOW)  # alternates to allow the stepper to run
+
+        sleep(.001)  # do not change
+        GPIO.output(ENABLE, GPIO.HIGH)  # stops power draw
+    sleep(1.0)
+    GPIO.output(DIR, CCW)  # sets the direction, give it time before switching direction
+    for x in range(400):  # change the range for how long it runs
+        GPIO.output(STEP, GPIO.HIGH)  # alternates to allow the stepper to run
+
+        sleep(.001)  # do not change
+
+        GPIO.output(STEP, GPIO.LOW)  # alternates to allow the stepper to run
+
+        sleep(.001)  # do not change
+        GPIO.output(ENABLE, GPIO.HIGH)  # stops power draw
